1884f9a5f1fde3ee8d3cd253b9837acbee34df3e
Brad Gessler <bradgessler@gmail.com>
2022-08-01 11:24:19 -0400

    Ignore Stripe configuration

--- config/subscriptions.rb
+++ config/subscriptions.rb
@@ -1,19 +1,21 @@
 def stripe_price_id(subscription)
   credentials = Rails.application.credentials
-  if Rails.env.development?
-    credentials.dig(:stripe, :price_ids, subscription) ||
-      "#{subscription}_dummy_stripe_price_id"
-  else
-    credentials.stripe[:price_ids][subscription]
-  end
+  # if Rails.env.development?
+  #   credentials.dig(:stripe, :price_ids, subscription) ||
+  #     "#{subscription}_dummy_stripe_price_id"
+  # else
+  #   credentials.stripe[:price_ids][subscription]
+  # end
+  "#{subscription}_dummy_stripe_price_id"
 end
 
 def revenue_cat_product_identifier(subscription)
   credentials = Rails.application.credentials
-  if Rails.env.development?
-    credentials.dig(:revenue_cat, :product_identifiers, subscription) ||
-      "#{subscription}_dummy_revenue_cat_product_identifier"
-  else
-    credentials.revenue_cat[:product_identifiers][subscription]
-  end
+  # if Rails.env.development?
+  #   credentials.dig(:revenue_cat, :product_identifiers, subscription) ||
+  #     "#{subscription}_dummy_revenue_cat_product_identifier"
+  # else
+  #   credentials.revenue_cat[:product_identifiers][subscription]
+  # end
+  "#{subscription}_dummy_revenue_cat_product_identifier"
 end
